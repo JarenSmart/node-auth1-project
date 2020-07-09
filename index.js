34db78f90601d496614e79c09359428abd19b726
jaren <jarensmart@yahoo.com>
2020-07-09 16:24:59 -0400

    Made initial folders, files, and db

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const usersRouter = require("./users/users-router");
+
+const server = express();
+const port = process.env.PORT || 6666;
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use(
+  session({
+    resave: false, // avoids recreating sessions that have not changed
+    saveUninitialized: false, // comply with GDPR laws
+    secret: "keep it secret, keep it safe",
+  })
+);
+
+server.use(usersRouter);
+
+server.use((err, req, res, next) => {
+  console.log(err);
+
+  res.status(500).json({
+    message: "Something went wrong",
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Running at http://localhost:${port}`);
+});
